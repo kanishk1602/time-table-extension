ccb93e75560b88a276920b033b203bc94fb45cd6
kanishk1602 <91908992+kanishk1602@users.noreply.github.com>
2022-04-18 22:20:52 +0530

    Add files via upload

---
+++ kanishk/ExternalJS.js
@@ -0,0 +1,116 @@
+var value=localStorage["m1"];
+document.getElementById("M1").innerHTML = value
+
+var value=localStorage["m2"];
+document.getElementById("M2").innerHTML = value
+
+var value=localStorage["m3"];
+document.getElementById("M3").innerHTML = value
+
+var value=localStorage["m4"];
+document.getElementById("M4").innerHTML = value
+
+var value=localStorage["m5"];
+document.getElementById("M5").innerHTML = value
+
+var value=localStorage["m6"];
+document.getElementById("T1").innerHTML = value
+
+var value=localStorage["m7"];
+document.getElementById("T2").innerHTML = value
+
+var value=localStorage["m8"];
+document.getElementById("T3").innerHTML = value
+
+var value=localStorage["m9"];
+document.getElementById("T4").innerHTML = value
+
+var value=localStorage["m10"];
+document.getElementById("W1").innerHTML = value
+
+var value=localStorage["m11"];
+document.getElementById("W2").innerHTML = value
+
+var value=localStorage["m12"];
+document.getElementById("Th1").innerHTML = value
+
+var value=localStorage["m13"];
+document.getElementById("Th2").innerHTML = value
+
+var value=localStorage["m14"];
+document.getElementById("Th3").innerHTML = value
+
+var value=localStorage["m15"];
+document.getElementById("Th4").innerHTML = value
+
+var value=localStorage["m16"];
+document.getElementById("Th5").innerHTML = value
+
+var value=localStorage["m17"];
+document.getElementById("Th6").innerHTML = value
+
+var value=localStorage["m18"];
+document.getElementById("Th7").innerHTML = value
+
+var value=localStorage["m19"];
+document.getElementById("F1").innerHTML = value
+
+var value=localStorage["m20"];
+document.getElementById("F2").innerHTML = value
+
+var value=localStorage["m21"];
+document.getElementById("F3").innerHTML = value
+
+var value=localStorage["m22"];
+document.getElementById("F4").innerHTML = value
+
+var value=localStorage["m23"];
+document.getElementById("St1").innerHTML = value
+
+var value=localStorage["m24"];
+document.getElementById("St2").innerHTML = value
+
+var value=localStorage["m25"];
+document.getElementById("St3").innerHTML = value
+
+var value=localStorage["m26"];
+document.getElementById("St4").innerHTML = value
+
+var value=localStorage["m27"];
+document.getElementById("St5").innerHTML = value
+
+var value=localStorage["m28"];
+document.getElementById("St6").innerHTML = value
+
+var value=localStorage["m29"];
+document.getElementById("St7").innerHTML = value
+
+var value=localStorage["m30"];
+document.getElementById("St8").innerHTML = value
+
+var value=localStorage["m31"];
+document.getElementById("St9").innerHTML = value
+
+var value=localStorage["m32"];
+document.getElementById("S1").innerHTML = value
+
+var value=localStorage["m33"];
+document.getElementById("S2").innerHTML = value
+
+var value=localStorage["m34"];
+document.getElementById("S3").innerHTML = value
+
+var value=localStorage["m35"];
+document.getElementById("S4").innerHTML = value
+
+var value=localStorage["m36"];
+document.getElementById("S5").innerHTML = value
+
+var value=localStorage["m37"];
+document.getElementById("S6").innerHTML = value
+
+var value=localStorage["m38"];
+document.getElementById("S7").innerHTML = value
+
+var value=localStorage["m39"];
+document.getElementById("S8").innerHTML = value
---
+++ kanishk/javascript.js
@@ -0,0 +1,422 @@
+// function fn1()
+// {
+//     var str= document.getElementById("a1").value;
+//     localStorage["m1"]=str
+// }
+
+// function fn2()
+// {
+//     var str= document.getElementById("a2").value;
+//     localStorage["m2"]=str
+// }
+
+// function fn3()
+// {
+//     var str= document.getElementById("a3").value;
+//     localStorage["m3"]=str
+// }
+
+// function fn4()
+// {
+//     var str= document.getElementById("a4").value;
+//     localStorage["m4"]=str
+// }
+
+// function fn5()
+// {
+//     var str= document.getElementById("a5").value;
+//     localStorage["m5"]=str
+// }
+
+// function fn6()
+// {
+//     var str= document.getElementById("b1").value;
+//     localStorage["m6"]=str
+// }
+
+// function fn7()
+// {
+//     var str= document.getElementById("b2").value;
+//     localStorage["m7"]=str
+// }
+
+// function fn8()
+// {
+//     var str= document.getElementById("b3").value;
+//     localStorage["m8"]=str
+// }
+
+// function fn9()
+// {
+//     var str= document.getElementById("b4").value;
+//     localStorage["m9"]=str
+// }
+
+// function fn10()
+// {
+//     var str= document.getElementById("c1").value;
+//     localStorage["m10"]=str
+// }
+
+// function fn11()
+// {
+//     var str= document.getElementById("c2").value;
+//     localStorage["m11"]=str
+// }
+
+// function fn12()
+// {
+//     var str= document.getElementById("d1").value;
+//     localStorage["m12"]=str
+// }
+
+// function fn13()
+// {
+//     var str= document.getElementById("d2").value;
+//     localStorage["m13"]=str
+// }
+
+// function fn14()
+// {
+//     var str= document.getElementById("d3").value;
+//     localStorage["m14"]=str
+// }
+
+// function fn15()
+// {
+//     var str= document.getElementById("d4").value;
+//     console.log(str);
+//     localStorage["m15"]=JSON.stringify(str);
+// }
+
+// function fn16()
+// {
+//     var str= document.getElementById("d5").value;
+//     localStorage["m16"]=str
+// }
+
+// function fn17()
+// {
+//     var str= document.getElementById("d6").value;
+//     localStorage["m17"]=str
+// }
+
+// function fn18()
+// {
+//     var str= document.getElementById("d7").value;
+//     localStorage["m18"]=str
+// }
+
+// function fn19()
+// {
+//     var str= document.getElementById("e1").value;
+//     localStorage["m19"]=str
+// }
+
+// function fn20()
+// {
+//     var str= document.getElementById("e2").value;
+//     localStorage["m20"]=str
+// }
+
+// function fn21()
+// {
+//     var str= document.getElementById("e3").value;
+//     localStorage["m21"]=str
+// }
+
+// function fn22()
+// {
+//     var str= document.getElementById("e4").value;
+//     localStorage["m22"]=str
+// }
+
+// function fn23()
+// {
+//     var str= document.getElementById("f1").value;
+//     localStorage["m23"]=str
+// }
+
+// function fn24()
+// {
+//     var str= document.getElementById("f2").value;
+//     localStorage["m24"]=str
+// }
+
+// function fn25()
+// {
+//     var str= document.getElementById("f3").value;
+//     localStorage["m25"]=str
+// }
+
+// function fn26()
+// {
+//     var str= document.getElementById("f4").value;
+//     localStorage["m26"]=str
+// }
+
+// function fn27()
+// {
+//     var str= document.getElementById("f5").value;
+//     localStorage["m27"]=str
+// }
+
+// function fn28()
+// {
+//     var str= document.getElementById("f6").value;
+//     localStorage["m28"]=str
+// }
+
+// function fn29()
+// {
+//     var str= document.getElementById("f8").value;
+//     localStorage["m29"]=str
+// }
+
+// function fn30()
+// {
+//     var str= document.getElementById("f9").value;
+//     localStorage["m30"]=str
+// }
+
+// function fn31()
+// {
+//     var str= document.getElementById("g1").value;
+//     localStorage["m31"]=str
+// }
+
+// function fn32()
+// {
+//     var str= document.getElementById("g2").value;
+//     localStorage["m32"]=str
+// }
+
+// function fn33()
+// {
+//     var str= document.getElementById("g3").value;
+//     localStorage["m33"]=str
+// }
+
+// function fn34()
+// {
+//     var str= document.getElementById("g4").value;
+//     localStorage["m34"]=str
+// }
+
+// function fn35()
+// {
+//     var str= document.getElementById("g5").value;
+//     localStorage["m35"]=str
+// }
+
+// function fn36()
+// {
+//     var str= document.getElementById("g6").value;
+//     localStorage["m36"]=str
+// }
+
+// function fn37()
+// {
+//     var str= document.getElementById("g7").value;
+//     localStorage["m37"]=str
+// }
+
+// function fn38()
+// {
+//     var str= document.getElementById("g8").value;
+//     localStorage["m38"]=str
+// }
+
+// function fn39()
+// {
+//     var str= document.getElementById("g8").value;
+//     localStorage["m39"]=str
+// }
+
+
+
+document.getElementById('btn1').addEventListener('click', (()=>{
+    var str= document.getElementById("a1").value;
+    localStorage["m1"]=str;
+}))
+
+document.getElementById('btn2').addEventListener('click', (()=>{
+    var str= document.getElementById("a2").value;
+    localStorage["m2"]=str;
+}))
+document.getElementById('btn3').addEventListener('click', (()=>{
+    var str= document.getElementById("a3").value;
+    localStorage["m3"]=str;
+}))
+
+document.getElementById('btn4').addEventListener('click', (()=>{
+    var str= document.getElementById("a4").value;
+    localStorage["m4"]=str;
+}))
+
+document.getElementById('btn5').addEventListener('click', (()=>{
+    var str= document.getElementById("a5").value;
+    localStorage["m5"]=str;
+}))
+
+document.getElementById('btn6').addEventListener('click', (()=>{
+    var str= document.getElementById("b1").value;
+    localStorage["m6"]=str;
+}))
+
+document.getElementById('btn7').addEventListener('click', (()=>{
+    var str= document.getElementById("b2").value;
+    localStorage["m7"]=str;
+}))
+
+document.getElementById('btn8').addEventListener('click', (()=>{
+    var str= document.getElementById("b3").value;
+    localStorage["m8"]=str;
+}))
+
+document.getElementById('btn9').addEventListener('click', (()=>{
+    var str= document.getElementById("b4").value;
+    localStorage["m9"]=str;
+}))
+
+document.getElementById('btn10').addEventListener('click', (()=>{
+    var str= document.getElementById("c1").value;
+    localStorage["m10"]=str;
+}))
+
+document.getElementById('btn11').addEventListener('click', (()=>{
+    var str= document.getElementById("c2").value;
+    localStorage["m11"]=str;
+}))
+
+document.getElementById('btn12').addEventListener('click', (()=>{
+    var str= document.getElementById("d1").value;
+    localStorage["m12"]=str;
+}))
+
+document.getElementById('btn13').addEventListener('click', (()=>{
+    var str= document.getElementById("d2").value;
+    localStorage["m13"]=str;
+}))
+
+document.getElementById('btn14').addEventListener('click', (()=>{
+    var str= document.getElementById("d3").value;
+    localStorage["m14"]=str;
+}))
+
+document.getElementById('btn15').addEventListener('click', (()=>{
+    var str= document.getElementById("d4").value;
+    localStorage["m15"]=str;
+}))
+
+document.getElementById('btn16').addEventListener('click', (()=>{
+    var str= document.getElementById("d5").value;
+    localStorage["m16"]=str;
+}))
+
+document.getElementById('btn17').addEventListener('click', (()=>{
+    var str= document.getElementById("d6").value;
+    localStorage["m17"]=str;
+}))
+
+document.getElementById('btn18').addEventListener('click', (()=>{
+    var str= document.getElementById("d7").value;
+    localStorage["m18"]=str;
+}))
+
+document.getElementById('btn19').addEventListener('click', (()=>{
+    var str= document.getElementById("e1").value;
+    localStorage["m19"]=str;
+}))
+
+document.getElementById('btn20').addEventListener('click', (()=>{
+    var str= document.getElementById("e2").value;
+    localStorage["m20"]=str;
+}))
+
+document.getElementById('btn21').addEventListener('click', (()=>{
+    var str= document.getElementById("e3").value;
+    localStorage["m21"]=str;
+}))
+
+document.getElementById('btn22').addEventListener('click', (()=>{
+    var str= document.getElementById("e4").value;
+    localStorage["m22"]=str;
+}))
+
+document.getElementById('btn23').addEventListener('click', (()=>{
+    var str= document.getElementById("f1").value;
+    localStorage["m23"]=str;
+}))
+
+document.getElementById('btn24').addEventListener('click', (()=>{
+    var str= document.getElementById("f2").value;
+    localStorage["m24"]=str;
+}))
+
+document.getElementById('btn25').addEventListener('click', (()=>{
+    var str= document.getElementById("f3").value;
+    localStorage["m25"]=str;
+}))
+
+document.getElementById('btn26').addEventListener('click', (()=>{
+    var str= document.getElementById("f4").value;
+    localStorage["m26"]=str;
+}))
+
+document.getElementById('btn27').addEventListener('click', (()=>{
+    var str= document.getElementById("f5").value;
+    localStorage["m27"]=str;
+}))
+
+document.getElementById('btn28').addEventListener('click', (()=>{
+    var str= document.getElementById("f6").value;
+    localStorage["m28"]=str;
+}))
+
+document.getElementById('btn29').addEventListener('click', (()=>{
+    var str= document.getElementById("f7").value;
+    localStorage["m29"]=str;
+}))
+
+document.getElementById('btn30').addEventListener('click', (()=>{
+    var str= document.getElementById("f8").value;
+    localStorage["m30"]=str;
+}))
+document.getElementById('btn31').addEventListener('click', (()=>{
+    var str= document.getElementById("f9").value;
+    localStorage["m31"]=str;
+}))
+document.getElementById('btn32').addEventListener('click', (()=>{
+    var str= document.getElementById("g1").value;
+    localStorage["m32"]=str;
+}))
+document.getElementById('btn33').addEventListener('click', (()=>{
+    var str= document.getElementById("g2").value;
+    localStorage["m33"]=str;
+}))
+document.getElementById('btn34').addEventListener('click', (()=>{
+    var str= document.getElementById("g3").value;
+    localStorage["m34"]=str;
+}))
+document.getElementById('btn35').addEventListener('click', (()=>{
+    var str= document.getElementById("g4").value;
+    localStorage["m35"]=str;
+}))
+document.getElementById('btn36').addEventListener('click', (()=>{
+    var str= document.getElementById("g5").value;
+    localStorage["m36"]=str;
+}))
+document.getElementById('btn37').addEventListener('click', (()=>{
+    var str= document.getElementById("g6").value;
+    localStorage["m37"]=str;
+}))
+document.getElementById('btn38').addEventListener('click', (()=>{
+    var str= document.getElementById("g7").value;
+    localStorage["m38"]=str;
+}))
+document.getElementById('btn39').addEventListener('click', (()=>{
+    var str= document.getElementById("g8").value;
+    localStorage["m39"]=str;
+}))
+
